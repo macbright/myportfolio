5e79518c930cde1b0c5605afbc7f776a5ec53da9
bright okike <brightokike@gmail.com>
2020-03-10 01:39:33 +0300

    added more project to the project tab

--- src/components/projects.js
+++ src/components/projects.js
@@ -60,6 +60,30 @@ class Projects extends Component {
 						 <IconButton name='share' />
 					 </CardMenu>
 				</Card>
+					<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+					 <CardTitle style={{color: '#fff', height: '176px', 
+					 	background: 'url(https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK.png) center / cover'}}>
+							Song List
+					 </CardTitle>
+					 <CardText>
+						 This project comes after the introduction of the React JS framework using Redux as state management. the main aim of 
+						 the project is to learn how to build a very simple React &amp; Redux application. 
+						 Tools: Node.js, React, React-DOM, React-Create-App, Redux, react-redux, npm, CSS, ES6
+					 </CardText>
+					 <CardActions>
+						 <a href="https://github.com/macbright/song-list-react-redux.git"
+								rel="noopener noreferrer" target="_blank" >
+								Github
+							</a>	
+							<a href="https://song-list.herokuapp.com/"
+								rel="noopener noreferrer" target="_blank" >
+								Live Demo
+							</a>
+					 </CardActions>
+					 <CardMenu style={{color: '#fff'}}>
+						 <IconButton name='share' />
+					 </CardMenu>
+				</Card>
 				</div>
 			)
 		} else if(this.state.activeTab === 1){
@@ -297,7 +321,147 @@ class Projects extends Component {
 			)
 		} else if(this.state.activeTab === 3){
 			return(
-				<div><h1> This is Ruby on Rails</h1></div>
+				<div>
+					<div className="projects-grid">
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									HTML FORMS
+							</CardTitle>
+							<CardText>
+								This project is about building a simple html website that has html form
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/macbright/mint-clone.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://macbright.github.io/mint-clone/"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									TNW-clone
+							</CardTitle>
+							<CardText>
+								In this project we got the chance to build a responsive website. We cloned The Next Web, "https://thenextweb.com/"
+								using media queries to gracefully break down their site as the window size is reduced. Check it out at
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/sumaerjolly/tnw-clone.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://sumaerjolly.github.io/tnw-clone/"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									TNW-clone
+							</CardTitle>
+							<CardText>
+								In this project we got the chance to build a responsive website. New York Times, 
+								using media queries to gracefully break down their site as the window size is reduced. 
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/macbright/new-york-times-clone.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://macbright.github.io/new-york-times-clone/#"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+					</div>
+					<div className="projects-grid second-row">
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									Apple Clone ( using our grid based framework)
+							</CardTitle>
+							<CardText>
+								In this project we created our own grid based framework using scss to clone https://www.apple.com/in/  
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/macbright/grid-based-framework.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://macbright.github.io/grid-based-framework/"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									Newsweek Clone (Bookstrap)
+							</CardTitle>
+							<CardText>
+								In this project i created a clone of Newsweek website using bootstrap framework 
+
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/macbright/newsweek-clone.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://macbright.github.io/newsweek-clone/"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+						<Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
+							<CardTitle style={{color: '#fff', height: '176px',
+								background: 'url(https://msatechnosoft.in/blog/wp-content/uploads/2017/04/Web-Design-using-HTML-msa-technosoft.png) center / cover'}}>
+									Response web (CSS GRID)
+							</CardTitle>
+							<CardText>
+								this is just a very simple responsive grid website i built during a course on CSS GRID 
+							</CardText>
+							<CardActions border>
+									<a href="https://github.com/macbright/simple-responsive-grid-website.git"
+										rel="noopener noreferrer" target="_blank" >
+										Github
+									</a>	
+									<a href="https://macbright.github.io/simple-responsive-grid-website/"
+										rel="noopener noreferrer" target="_blank" >
+										Live Demo
+									</a>
+							</CardActions>
+							<CardMenu style={{color: '#fff'}}>
+								<IconButton name='share' />
+							</CardMenu>
+						</Card>
+					</div>
+				</div>
 			)
 		} 
 	}
